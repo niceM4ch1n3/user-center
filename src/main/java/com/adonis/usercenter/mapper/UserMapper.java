69a1e79e4940f5159dde3cc46ab1e87c7ebb9a06
451821472@qq.com <451821472@qq.com>
2023-12-05 22:40:17 +0800

    init project

---
+++ src/main/java/com/adonis/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.adonis.usercenter.mapper;
+
+import com.adonis.usercenter.model.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/test/java/com/adonis/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,25 @@
+package com.adonis.usercenter;
+
+import com.adonis.usercenter.mapper.UserMapper;
+import com.adonis.usercenter.model.User;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class UserCenterApplicationTests {
+    @Resource
+    private UserMapper userMapper;
+
+    @Test
+    void contextLoads() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
+    }
+
+}
